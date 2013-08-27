58ffecfcc0b47439693cc429f9d06b9f4fce2a4f
Adam L. Taylor <adamlyletaylor@gmail.com>
2013-08-27 14:42:53 -0400

    Still working on methods figure 1.  Seemed like a good time to checkpoint.

--- modpath.m
+++ modpath.m
@@ -7,5 +7,7 @@ addpath(genpath(fullfile(this_dir_name,'toolbox','tmt_rel_114')));
 addpath(genpath(fullfile(this_dir_name,'toolbox','snippeter')));
 %addpath(genpath(fullfile(this_dir_name,'taylor_matlab_toolbox/113')));
 %addpath(genpath(fullfile(this_dir_name,'groundswell/groundswell_rel_118')));
+parent_dir_name=fileparts(this_dir_name);
+addpath(degit(genpath(fullfile(parent_dir_name,'ax_repo'))));
 
 end
---
+++ toolbox/r_est_from_segment_indicators.m
@@ -0,0 +1,88 @@
+function [r_est_blobs,overhead]= ...
+  r_est_from_segment_indicators(base_dir_name,...
+                                data_analysis_dir_name, ...
+                                date_str, ...
+                                letter_str, ...
+                                i_segment, ...
+                                args_template, ...
+                                verbosity)
+
+% do all the stuff we only have to do once per trial
+[syl_name_all,i_start_all,i_end_all,f_lo_all,f_hi_all, ...
+ r_head_all,r_tail_all,R,Temp, ...
+ dx,x_grid,y_grid,in_cage]= ...
+  ssl_trial_overhead(base_dir_name, ...
+                     data_analysis_dir_name, ...
+                     date_str, ...
+                     letter_str);
+%n_voc=length(i_syl_all);
+
+% filter out the syl's that don't match i_segment
+if ~isempty(syl_name_all) ,
+  syl_name_all_first=syl_name_all{1};
+  if length(syl_name_all_first)~=17 ,
+    error('The "syllable" "names" appear to be in the wrong format.');
+  end
+  i_segment_all_as_string=cellfun(@(s)s(4:9),syl_name_all,'UniformOutput',false);
+  i_segment_all=str2double(i_segment_all_as_string);
+  keep=(i_segment_all==i_segment);
+  syl_name_keep=syl_name_all(keep);
+  i_start_keep=i_start_all(keep);
+  i_end_keep=i_end_all(keep);
+  f_lo_keep=f_lo_all(keep);
+  f_hi_keep=f_hi_all(keep);
+  r_head_keep=r_head_all(:,keep,:);
+  r_tail_keep=r_tail_all(:,keep,:);    
+end
+
+% package up all the trial overhead
+overhead.R=R;
+overhead.Temp=Temp;
+overhead.dx=dx;
+overhead.x_grid=x_grid;
+overhead.y_grid=y_grid;
+overhead.in_cage=in_cage;
+
+% pack up the arguments that don't change across snippets
+args_template_and_overhead=merge_scalar_structs(args_template,overhead);
+
+% do the estimation for each snippet
+n_snippets=length(syl_name_keep);
+r_est_blobs=struct();
+for i_snippet=1:n_snippets ,
+  % pack up all the arguments
+  snippet_args=args_template_and_overhead;
+  snippet_args.syl_name=syl_name_keep{i_snippet};
+  snippet_args.i_start=i_start_keep(i_snippet);
+  snippet_args.i_end=i_end_keep(i_snippet);  
+  snippet_args.f_lo=f_lo_keep(i_snippet);  
+  snippet_args.f_hi=f_hi_keep(i_snippet);  
+  snippet_args.r_head=r_head_keep(:,i_snippet);  
+  snippet_args.r_tail=r_tail_keep(:,i_snippet);
+
+  % estimate r
+  r_est_blob_this = ... 
+    r_est_from_voc_indicators_and_ancillary(base_dir_name, ...
+                                            date_str, ...
+                                            letter_str, ...
+                                            snippet_args, ...
+                                            verbosity);
+
+  % throw some args into the returned blob
+  %args_field_name=fieldnames(args);
+  args_field_names_to_add_to_blob={'syl_name' 'i_start' 'i_end' 'f_lo' 'f_hi' 'r_head' 'r_tail'}';
+  for i=1:length(args_field_names_to_add_to_blob)
+    r_est_blob_this.(args_field_names_to_add_to_blob{i})=snippet_args.(args_field_names_to_add_to_blob{i});
+  end
+  
+  if i_snippet==1
+    r_est_blobs=r_est_blob_this;
+    fs=r_est_blob_this.fs;
+    overhead.fs=fs;
+  else
+    r_est_blobs(i_snippet)=r_est_blob_this;
+  end
+end
+
+end
+
